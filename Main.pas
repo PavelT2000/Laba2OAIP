3f0eb434af102ae8f1f090a842766a2e66b34311
watari1337 <alexboyip@gmail.com>
2025-03-02 18:29:13 +0300

    Reapply "Merge branch 'main' of https://github.com/PavelT2000/Laba2OAIP"
    This reverts commit 190c685dda4ae8274a28509f6b9ed305aca4043f.

--- Main.pas
+++ Main.pas
@@ -1,5 +1,6 @@
 unit Main;
 
+
 {$R *.dfm}
 
 interface
@@ -16,11 +17,16 @@ uses
 type
   TForm1 = class(TForm)
     MediaPlayer1: TMediaPlayer;
-    PaintBox1: TPaintBox;
     FPS: TTimer;
+    PaintBox1: TPaintBox;
     procedure FormPaint(Sender: TObject);
-    procedure PaintBox1MouseMove(Sender: TObject; Shift: TShiftState;
-      X, Y: Integer);
+    procedure FormCreate(Sender: TObject);
+    procedure PaintBox1MouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure PaintBox1MouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
   private
     { Private declarations }
   public
@@ -32,7 +38,9 @@ var
 
 
 implementation
-var Location: TPoint;
+var Location: TPointF;
+var StartDrag, StopDrag: TPointF;
+var IsDragging: Boolean;
 
 procedure Paint1(Canvas: TCanvas; x1, y1, x2, y2: Integer);
 begin
@@ -43,17 +51,28 @@ begin
   Canvas.LineTo(x2, y2);
 end;
 
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Location := point(0, 0);
+  StopDrag := point(0, 0);
+  IsDragging := false;
+  // drawSomeThing.DrawHand(Canvas, RightHandPos1);
+  // drawSomeThing.DrawPerson();
+  // Canvas.Draw(0, 0);
+end;
+
 procedure TForm1.FormPaint(Sender: TObject);
 var
   grad: Integer;
   centerPoint: TPoint;
+  FormRect: TRect;
 begin
   drawSomeThing.SetCanvas(Canvas);
-  Location := point(0, 0);
 
   //Music.TurnOn(CalmMind);
-  DrawLocation(Form1.Canvas, Location);
-  centerPoint := point(Round(Form1.ClientWidth / 2),
+  FormRect := Rect(0, 0, ClientWidth, ClientHeight);
+  DrawLocation(Canvas, Location, FormRect);
+  centerPoint := point(Round(ClientWidth / 2),
     Round(Form1.ClientHeight / 2));
   drawSomeThing.DrawPerson(RightHandPos1, LeftHandPos1, RightLegPos1,
     LeftLegPos1, centerPoint, 1.7);
@@ -65,10 +84,31 @@ begin
     Paint1(Canvas, 100, 100, 200, 500); }
 end;
 
-procedure TForm1.PaintBox1MouseMove(Sender: TObject; Shift: TShiftState;
-  X, Y: Integer);
+procedure TForm1.PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
 begin
-  Location := Location + point(X, Y);
+  // Точка, где начинается перетаскивание локации
+  StartDrag := PointF(X/ ClientWidth, Y/ ClientHeight);
+  IsDragging := true;
+end;
+
+procedure TForm1.PaintBox1MouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  if IsDragging then
+  begin
+    // Берем координаты локации и прибавляем к ней координаты перетащенного курсора
+    Location := pointF(StopDrag.X + X/ ClientWidth - StartDrag.X, StopDrag.Y + Y/ ClientHeight - StartDrag.Y);
+    PaintBox1.Invalidate;
+  end;
+end;
+
+procedure TForm1.PaintBox1MouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+   IsDragging := false;
+   // Записываем где сейчас остановилась локация, чтобы потом с этого места ее перетаскивать
+   StopDrag := PointF(StopDrag.X + X / ClientWidth - StartDrag.X, StopDrag.Y + Y / ClientHeight - StartDrag.Y);
 end;
 
 end.
