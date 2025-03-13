339e9e847c736bd9305c50fad3fd613c5d43609c
watari1337 <alexboyip@gmail.com>
2025-03-13 09:59:07 +0300

    move screen

--- Main.pas
+++ Main.pas
@@ -98,7 +98,7 @@ begin
   end;
 
   //Music.TurnOn(CalmMind);
-  allCadrs:= 0;
+  allCadrs:= 260;
 end;
 
 procedure NextPaint();
@@ -123,6 +123,16 @@ begin
 
   if (allCadrs > 0) and (allCadrs <= NikTestMaxCadrsCount) then NikManTest.DrawPerson(allCadrs);
 
+  //едет на трассу
+  if (allCadrs > 260) and (allCadrs <= 380) then PLocation:= PLocation + PointF(-0.003, 0);
+  //разгоняется
+  if (allCadrs > 380) and (allCadrs <= 460) then PLocation:= PLocation + PointF(-0.012, -0.0055);
+  //начало полёта
+  if (allCadrs > 525) and (allCadrs <= 600) then PLocation:= PLocation + PointF(-0.004, -0.0018);
+  //летит
+  if (allCadrs > 600) and (allCadrs <= 800) then PLocation:= PLocation + PointF(-0.007, -0.005);
+  //приземлился
+  if (allCadrs > 800) and (allCadrs <= 850) then PLocation:= PLocation + PointF(-0.003, 0);
 
   // mod не обязателен, он для повторения анимации
   //NikManTest.DrawPerson(allCadrs+1);
